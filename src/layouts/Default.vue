4e5c8719d6db0b4d98fbe5b6bd01120d1d6416af
David Sabalete Rodríguez <david.sabalete@lovetoknow.com>
2020-12-10 10:37:04 +0100

    initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,73 @@
+<template>
+    <v-app>
+        <v-navigation-drawer width="250px" permanent>
+            <v-list-item>
+                <v-list-item-avatar>
+                    <img
+                        src="https://avatars3.githubusercontent.com/u/57623874?s=460&u=bcb4dcb2225b8b904322be5c317dac8cc1347dfb&v=4"
+                    />
+                </v-list-item-avatar>
+                <v-list-item-content>
+                    <v-list-item-title class="title">
+                        David Sabalete
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        Software Developer
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list dense nav>
+                <v-list-item v-for="item in items" :key="item.title" link>
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-main>
+            <v-container>
+                <slot />
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                { title: 'About', icon: 'mdi-account-box-outline' },
+                { title: 'Projects', icon: 'mdi-apps' },
+                { title: 'Education', icon: 'mdi-school-outline' },
+                { title: 'Contact', icon: 'mdi-message-text-outline' },
+            ],
+            right: null,
+        }
+    },
+}
+</script>
+
+<style>
+.v-application--wrap {
+    flex-direction: row;
+}
+.v-main {
+    flex: 1 0;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+    <Layout>
+        <section>
+            <v-container>
+                <v-row>
+                    <v-col sm="6">
+                        <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+                        <g-image
+                            alt="Example image"
+                            src="~/profile.jpg"
+                            width="500"
+                        />
+                    </v-col>
+                    <v-col sm="6">
+                        <h1>Hi, I'm David.</h1>
+                        <p>I'm a software developer...</p>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </section>
+    </Layout>
+</template>
+
+<script>
+export default {
+    metaInfo: {
+        title: 'Hello, world!',
+    },
+}
+</script>
+
+<style>
+.home-links a {
+    margin-right: 1rem;
+}
+</style>
